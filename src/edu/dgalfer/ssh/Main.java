7a6039bd92752bae1b6a4d364a189db567d4e8f7
L3CR0NN <danigfernandez1@gmail.com>
2025-11-12 09:17:39 +0100

    Proyecto Java creado desde Eclipse y gestionado por terminal (SSH listo)

---
+++ src/edu/dgalfer/ssh/Main.java
@@ -0,0 +1,23 @@
+package edu.dgalfer.ssh;
+
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		//Investiga el uso de random y genera por pantalla los números de la lotería primitiva.
+        Random rnd = new Random();
+        // Genera 5 números principales
+        int p1 = rnd.nextInt(0,4)+1;
+        int p2 = rnd.nextInt(0,4)+1;
+        int p3 = rnd.nextInt(0,4)+1;
+        int p4 = rnd.nextInt(0,4)+1;
+        int p5 = rnd.nextInt(0,4)+1;
+        
+        
+        // Número complementario
+        int c = rnd.nextInt(0,20)+1;
+        System.out.println("Números de la Lotería Primitiva:" + p1 + p2 + p3 + p4 + p5+c);
+	}
+
+}
