602bb8a7aabca2aa0739eb50e29755466ceb5017
L3CR0NN <danigfernandez1@gmail.com>
2025-11-13 10:20:18 +0100

    Proyecto Java creado desde Eclipse y gestionado por terminal (SSH listo)

--- src/edu/dgalfer/ssh/Main.java
+++ src/edu/dgalfer/ssh/Main.java
@@ -18,6 +18,7 @@ public class Main {
         // Número complementario
         int c = rnd.nextInt(0,20)+1;
         System.out.println("Números de la Lotería Primitiva:" + p1 + p2 + p3 + p4 + p5+c);
+        System.out.println("¡Buena suerte en el sorteo!");
 	}
 
 }
