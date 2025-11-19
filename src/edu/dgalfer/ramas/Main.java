df6b0eeeb528be227947b360e5b4137d42259e7d
L3CR0NN <danigfernandez1@gmail.com>
2025-11-19 09:10:59 +0100

    Mensaje en consola (rama feature/mensaje)

--- src/edu/dgalfer/ramas/Main.java
+++ src/edu/dgalfer/ramas/Main.java
@@ -14,5 +14,8 @@ public class Main {
         System.out.println("Números principales: " + bolas);
         int complementario = r.nextInt(49) + 1;
         System.out.println("Complementario: " + complementario);
+        int suma = bolas.stream().mapToInt(Integer::intValue).sum();
+        System.out.println("Suma de principales: " + suma);
+        System.out.println("¡Suerte en el sorteo, DAM!");
     }
 }
